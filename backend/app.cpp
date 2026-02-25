0f93cbedebd4cd69b1d0f9e6e6b539f43f2c66f4
PES1UG23CS906 <pes1ug23me051@pesu.pes.edu>
2026-02-25 16:48:24 +0000

    Fixed backend and nginx for load balancing PES1UG23CS906

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <unistd.h>
+#include <limits.h>
+
 using namespace std;
 
 int main() {
-    cout << "Hello from PES1UG23CS906 Jenkins CI/CD Lab!" << endl;
+    char hostname[HOST_NAME_MAX];
+    gethostname(hostname, HOST_NAME_MAX);
+
+    cout << "Served by backend: " << hostname << endl;
+
     return 0;
 }
